81bc14a95c2f4678284c7b954fa2a2ef6255faea
favour chukwuebuka <favorwinner1@gmail.com>
2023-03-19 20:08:11 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("track-button");
+let = document.getElementById("hello-track");
+
+
+btn.addEventListener("click", () =>{
+    let.classList.toggle("show");
+})
+
